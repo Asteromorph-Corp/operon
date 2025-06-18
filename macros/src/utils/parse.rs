a3ae410d25c43d94aa2bea627462f9702f3c1f34
crescmoon <crescentmoon8569@gmail.com>
2025-06-18 21:21:26 +0900

    feat: remove file configs in favour of function-like configurations

--- macros/src/utils/parse.rs
+++ macros/src/utils/parse.rs
@@ -1,28 +1,51 @@
-use indexmap::IndexMap;
-use once_cell::sync::Lazy;
-use serde::Deserialize;
-use std::path::PathBuf;
 use super::types::*;
+use indexmap::IndexMap;
+use quote::ToTokens;
+use syn::{
+    braced, bracketed, parse::{Parse, ParseStream}, punctuated::Punctuated, ExprAssign, Token
+};
 
-static CONFIG_PATH: Lazy<PathBuf> = Lazy::new(get_config_path);
-
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Default)]
 struct RawEntity {
     primary: Option<bool>,
-    #[serde(rename = "type")]
-    r#type: String,
-    dimensions: Vec<String>,
-    definition: Option<String>,
+    dims: Vec<String>,
+    def: Option<String>,
     from: Option<Vec<String>>,
     pool: Option<usize>,
+    body: String,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Default)]
 struct RawTypesConfig {
     entity: IndexMap<String, RawEntity>,
 }
+impl Parse for RawTypesConfig {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let mut types = RawTypesConfig::default();
+        while !input.is_empty() {
+            let _ = input.parse::<Token![#]>()?;
+            let attr_content;
+            let _ = bracketed!(attr_content in input);
+            let attr_type = attr_content.parse::<syn::Ident>()?;
+            if attr_type != "entity" {
+                return Err(syn::Error::new_spanned(attr_type, "Expected `entity` attribute"));
+            }
+            while !attr_content.is_empty() {
+                let arg_name = attr_content.parse::<syn::Ident>()?;
+                let _ = attr_content.parse::<Token![=]>()?;
+                match arg_name.to_string().as_str() {
+                    _ => todo!()
+                }
+            }
 
-#[derive(Debug, Deserialize, Default)]
+
+            let _ = input.parse::<Token![;]>()?;
+        }
+        Ok(types)
+    }
+}
+
+#[derive(Debug, Default)]
 struct RawLogConfig {
     level: Option<String>,
     buffer_size: Option<usize>,
@@ -30,65 +53,123 @@ struct RawLogConfig {
     dump_path: Option<String>,
 }
 
-#[derive(Debug, Deserialize)]
-struct RawConfig {
+#[derive(Debug, Default)]
+struct RawCommonConfig {
     storage: StorageConfig,
-    log: Option<RawLogConfig>,
+    log: RawLogConfig,
 }
-
-fn read_types_from_str(types_config: &str) -> RawTypesConfig {
-    RawTypesConfig::deserialize(toml::de::Deserializer::new(types_config))
-        .expect("Failed to parse types file")
-}
-fn read_config_from_str(config: &str) -> RawConfig {
-    RawConfig::deserialize(toml::de::Deserializer::new(config))
-        .expect("Failed to parse config file")
-}
-
-fn get_config_path() -> PathBuf {
-    let mut path = std::env::var("CARGO_MANIFEST_DIR")
-        .map(PathBuf::from)
-        .expect("CARGO_MANIFEST_DIR not set");
-    path.push("config");
-    assert!(
-        path.exists(),
-        "Config directory does not exist at {}",
-        path.display()
-    );
-    path
-}
-
-fn parse_entities(raw: RawTypesConfig) -> Entities {
-    let mut entities = IndexMap::new();
-
-    // TODO
-
-    entities
-}
-fn parse_config(raw: RawConfig) -> GlobalConfig {
-    let log = raw.log.unwrap_or_default();
-    let log_config = LogConfig {
-        level: log.level.unwrap_or("info".to_string()),
-        buffer_size: log.buffer_size.unwrap_or(1024),
-        dump: log.dump.unwrap_or(false),
-        dump_path: CONFIG_PATH.join(log.dump_path.unwrap_or("logs".to_string())),
-    };
-
-    GlobalConfig {
-        storage: raw.storage,
-        log: log_config,
+impl Parse for RawCommonConfig {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let mut config = RawCommonConfig::default();
+        while !input.is_empty() {
+            let arg: ExprAssign = input.parse()?;
+            match arg.left.to_token_stream().to_string().as_str() {
+                "storage.data.uri" => {
+                    config.storage.data.uri = arg.right.to_token_stream().to_string();
+                }
+                "storage.data.schema" => {
+                    config.storage.data.schema = Some(arg.right.to_token_stream().to_string());
+                }
+                "storage.data.pool_size" => {
+                    config.storage.data.pool_size =
+                        Some(arg.right.to_token_stream().to_string().parse().unwrap());
+                }
+                "storage.metadata.uri" => {
+                    config.storage.metadata.uri = arg.right.to_token_stream().to_string();
+                }
+                "storage.metadata.schema" => {
+                    config.storage.metadata.schema = Some(arg.right.to_token_stream().to_string());
+                }
+                "storage.metadata.pool_size" => {
+                    config.storage.metadata.pool_size =
+                        Some(arg.right.to_token_stream().to_string().parse().unwrap());
+                }
+                "log.level" => {
+                    config.log.level = Some(arg.right.to_token_stream().to_string());
+                }
+                "log.buffer_size" => {
+                    config.log.buffer_size =
+                        Some(arg.right.to_token_stream().to_string().parse().unwrap());
+                }
+                "log.dump" => {
+                    config.log.dump =
+                        Some(arg.right.to_token_stream().to_string().parse().unwrap());
+                }
+                "log.dump_path" => {
+                    config.log.dump_path = Some(arg.right.to_token_stream().to_string());
+                }
+                _ => return Err(syn::Error::new_spanned(arg, "Unknown configuration option")),
+            }
+            let _ = input.parse::<Token![,]>();
+        }
+        Ok(config)
     }
 }
 
-pub fn get_entities() -> Entities {
-    let content =
-        std::fs::read_to_string(CONFIG_PATH.join("types.toml")).expect("Couldn't read types.toml");
-    let raw = read_types_from_str(&content);
-    parse_entities(raw)
+#[derive(Debug, Default)]
+struct RawAllConfig {
+    types: RawTypesConfig,
+    global: RawCommonConfig,
+    use_psql_storage: Option<String>,
 }
-pub fn get_config() -> GlobalConfig {
-    let content = std::fs::read_to_string(CONFIG_PATH.join("config.toml"))
-        .expect("Couldn't read config.toml");
-    let raw = read_config_from_str(&content);
-    parse_config(raw)
+impl Parse for RawAllConfig {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let mut all_configs = RawAllConfig::default();
+        while !input.is_empty() {
+            let ident = input.parse::<syn::Ident>()?;
+            match ident.to_string().as_str() {
+                "config" => {
+                    let content;
+                    let _ = braced!(content in input);
+                    all_configs.global = content.parse()?;
+                }
+                "types" => {
+                    let content;
+                    let _ = braced!(content in input);
+                    all_configs.types = content.parse()?;
+                }
+                "use_psql_storage" => {
+                    all_configs.use_psql_storage = Some(input.parse::<syn::Ident>()?.to_string());
+                }
+                _ => {
+                    return Err(syn::Error::new_spanned(
+                        ident,
+                        "Unknown configuration section",
+                    ));
+                }
+            }
+            let _ = input.parse::<Token![;]>();
+        }
+        Ok(all_configs)
+    }
+}
+
+fn resolve_types(raw: RawTypesConfig) -> TypesConfig {
+    let mut entities: Entities = Vec::new();
+    let mut dimensions: Dimensions = Vec::new();
+    let mut jobs: Jobs = Vec::new();
+
+    for (i, (entity_name, raw_entity)) in raw.entity.iter().enumerate() {
+        let primary = match raw_entity.primary {
+            Some(true) if i == 0 => true,
+            Some(true) => {
+                panic!(
+                    "Only the first entity can be primary, but {entity_name} was marked as primary"
+                )
+            }
+            Some(false) if i == 0 => {
+                panic!(
+                    "The first entity must be primary, but {entity_name} was marked as non-primary"
+                )
+            }
+            Some(false) => false,
+            None => i == 0,
+        };
+    }
+
+    TypesConfig {
+        entities,
+        dimensions,
+        jobs,
+    }
 }
--- macros/src/utils/types.rs
+++ macros/src/utils/types.rs
@@ -1,37 +1,110 @@
-use std::{path::PathBuf, sync::Arc};
-use serde::Deserialize;
-use indexmap::IndexMap;
+use std::path::PathBuf;
 
+use proc_macro2::TokenStream;
+
+// Parsed from `types`:
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct DimensionTag(pub usize);
 #[derive(Debug)]
 pub struct Dimension {
+    pub tag: DimensionTag,
     pub name: String,
     pub primary: bool,
+    pub depends_on: Vec<DimensionTag>,
 }
+#[allow(dead_code)]
+impl Dimension {
+    pub fn get_by_tag(dimensions: &Dimensions, tag: DimensionTag) -> Option<&Dimension> {
+        let dimension = dimensions.get(tag.0);
+        if dimension.is_some() {
+            assert_eq!(dimension.unwrap().tag, tag, "Dimension tag mismatch");
+        }
+        dimension
+    }
+    pub fn get_by_name<'a> (dimensions: &'a Dimensions, name: &str) -> Option<&'a Dimension> {
+        dimensions.iter().find(|d| d.name == name)
+    }
+}
+pub type Dimensions = Vec<Dimension>;
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct EntityTag(pub usize);
 #[derive(Debug)]
 pub struct Entity {
+    pub tag: EntityTag,
     pub name: String,
     pub primary: bool,
-    pub typedef: String,
-    pub dimensions: Vec<Dimension>,
-    pub definition: Option<(String, Vec<Dimension>)>,
-    pub from: Option<Vec<(Arc<Entity>, Vec<Dimension>)>>,
+    pub dimensions: Vec<DimensionTag>,
+    pub definition: Option<(JobTag, Vec<DimensionTag>)>,
+    pub from: Option<Vec<(EntityTag, Vec<DimensionTag>)>>,
     pub pool: Option<usize>,
+    pub body: String,
 }
-pub type Entities = IndexMap<String, Entity>;
+#[allow(dead_code)]
+impl Entity {
+    pub fn get_by_tag(entities: &Entities, tag: EntityTag) -> Option<&Entity> {
+        let entity = entities.get(tag.0);
+        if entity.is_some() {
+            assert_eq!(entity.unwrap().tag, tag, "Entity tag mismatch");
+        }
+        entity
+    }
+    pub fn get_by_name<'a> (entities: &'a Entities, name: &str) -> Option<&'a Entity> {
+        entities.iter().find(|e| e.name == name)
+    }
+}
+pub type Entities = Vec<Entity>;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct JobTag(pub usize);
+#[derive(Debug)]
+pub struct Job {
+    pub tag: JobTag,
+    pub name: String,
+    pub repeats_on: Vec<DimensionTag>,
+    pub from: Vec<(EntityTag, Vec<DimensionTag>)>,
+    pub defines: (EntityTag, Vec<DimensionTag>),
+    pub spawns: Option<DimensionTag>,
+    pub pool: usize,
+}
+impl Job {
+    pub fn get_by_tag(jobs: &Jobs, tag: JobTag) -> Option<&Job> {
+        let job = jobs.get(tag.0);
+        if job.is_some() {
+            assert_eq!(job.unwrap().tag, tag, "Job tag mismatch");
+        }
+        job
+    }
+    pub fn get_by_name<'a> (jobs: &'a Jobs, name: &str) -> Option<&'a Job> {
+        jobs.iter().find(|j| j.name == name)
+    }
+}
+pub type Jobs = Vec<Job>;
+
+#[derive(Debug)]
+pub struct TypesConfig {
+    pub entities: Entities,
+    pub dimensions: Dimensions,
+    pub jobs: Jobs,
+}
+
+// Parsed from `config`:
+
+#[derive(Debug, Default)]
 pub struct Connection {
     pub uri: String,
     pub schema: Option<String>,
+    pub pool_size: Option<usize>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Default)]
 pub struct StorageConfig {
     pub metadata: Connection,
     pub data: Connection,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct LogConfig {
     pub level: String,
     pub buffer_size: usize,
@@ -39,8 +112,8 @@ pub struct LogConfig {
     pub dump_path: PathBuf,
 }
 
-#[derive(Debug)]
-pub struct GlobalConfig {
+#[derive(Debug, Default)]
+pub struct CommonConfig {
     pub storage: StorageConfig,
     pub log: LogConfig,
 }
