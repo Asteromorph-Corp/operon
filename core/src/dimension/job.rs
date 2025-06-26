5f1ece6335ab9a9c793f7f857cde414c79f1b449
coinmoles <andrewpark421@gmail.com>
2025-06-26 19:04:24 +0900

    chore: remove unused `JobEnum` and `ResolutionEnum` traits

--- core/src/dimension/job.rs
+++ core/src/dimension/job.rs
@@ -1,5 +1,3 @@
 use std::fmt::Debug;
 
-pub trait JobEnum: Debug + Clone + Send + Sync + 'static {}
-
 pub trait Job: Debug + Clone + Send + Sync + 'static {}
--- core/src/dimension/resolution.rs
+++ core/src/dimension/resolution.rs
@@ -1,7 +1,3 @@
 use std::fmt::Debug;
 
-pub trait ResolutionEnum: Debug + Clone + Send + Sync + 'static {
-    fn primary(ub: usize) -> Self;
-}
-
 pub trait Resolution: Debug + Clone + Send + Sync + 'static {}
