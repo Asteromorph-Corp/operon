4b112a27bf92d4ba624e2fe0fc1c071f41d4d0e9
coinmoles <andrewpark421@gmail.com>
2025-06-30 15:51:54 +0900

    chore: separate events into an independent module

---
+++ core/src/scheduler/events/mod.rs
@@ -0,0 +1,11 @@
+mod recovery_state;
+pub use recovery_state::*;
+
+mod control_event;
+pub use control_event::*;
+
+mod peer_event;
+pub use peer_event::*;
+
+mod internal_event;
+pub use internal_event::*;
--- core/src/scheduler/mod.rs
+++ core/src/scheduler/mod.rs
@@ -8,20 +8,11 @@ pub use individual_scheduler::*;
 mod scheduler_spec;
 pub use scheduler_spec::*;
 
-mod recovery_state;
-pub use recovery_state::*;
-
-mod control_event;
-pub use control_event::*;
-
 mod run_mode;
 pub use run_mode::*;
 
-mod peer_event;
-pub use peer_event::*;
-
-mod internal_event;
-pub use internal_event::*;
+mod events;
+pub use events::*;
 
 mod options;
 pub use options::*;
