88f46e238a212b5002019ff833da214c4975ef02
coinmoles <andrewpark421@gmail.com>
2025-07-02 07:51:49 +0000

    feat: clean up config structs

---
+++ macros/src/configs/entity.rs
@@ -0,0 +1,14 @@
+use crate::configs::DimensionId;
+
+pub type EntityId = String;
+
+/// An Operon entity, which can be a struct or an enum.
+#[derive(Debug)]
+pub struct EntityConfig {
+    /// Unique identifier for the dimension.
+    pub id: EntityId,
+    /// Dimensions this entity repeat on.
+    pub dims: Vec<DimensionId>,
+    /// The Rust definition of this entity.
+    pub body: String,
+}
