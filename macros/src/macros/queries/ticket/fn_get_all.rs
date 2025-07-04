a22f9549db38eebc3dbf6fb87dc0514fa747426f
coinmoles <andrewpark421@gmail.com>
2025-07-04 07:25:59 +0000

    feat: implement `get_all` generation

---
+++ macros/src/macros/queries/ticket/fn_get_all.rs
@@ -0,0 +1,88 @@
+use quote::quote;
+
+use crate::{
+    configs::JobConfig,
+    utils::{get_all_ident, operon_ident, ticket_ident},
+};
+
+/// Helper struct to generate the SQL query for getting all tickets for a given job.
+struct GetAllTicketQuery<'a>(&'a JobConfig);
+
+impl std::fmt::Display for GetAllTicketQuery<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "SELECT * FROM {{schema_prefix}}ticket_{} WHERE status = $1;",
+            self.0.id
+        )
+    }
+}
+
+pub(super) fn fn_get_all(job: &JobConfig) -> proc_macro2::TokenStream {
+    let operon = operon_ident();
+    let fn_name = get_all_ident(&job.id);
+    let ticket_ident = ticket_ident(&job.id);
+    let stmt = GetAllTicketQuery(job).to_string();
+
+    quote! {
+        pub async fn #fn_name(
+            client: #operon::meta_storage::MetaClient<'_>,
+            status: #operon::schema_base::TicketStatus,
+        ) -> Result<Vec<#ticket_ident>, #operon::meta_storage::MetaStorageError> {
+            let schema_prefix = client.schema_prefix();
+            let stmt = format!(#stmt);
+            let rows = client.query(&stmt, &[&status]).await?;
+            let jobs = rows.
+                iter()
+                .map(|row| <#ticket_ident as #operon::schema_base::TicketSql>::from_sql_row(row))
+                .collect::<Result<Vec<_>, _>>()?;
+            Ok(jobs)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_all_ticket_query() {
+        let job = JobConfig {
+            id: "beta".to_string(),
+            from: vec!["a".to_string()],
+            to: "b".to_string(),
+            dims: vec!["dim1".to_string(), "dim2".to_string()],
+        };
+        let query = GetAllTicketQuery(&job).to_string();
+        let expected = "SELECT * FROM {schema_prefix}ticket_beta WHERE status = $1;";
+        assert_eq!(query, expected);
+    }
+
+    #[test]
+    fn test_fn_get_all() {
+        let job = JobConfig {
+            id: "beta".to_string(),
+            from: vec!["a".to_string()],
+            to: "b".to_string(),
+            dims: vec!["dim1".to_string(), "dim2".to_string()],
+        };
+        let tokens = fn_get_all(&job);
+        let expected = quote! {
+            pub async fn get_all_beta(
+                client: operon::meta_storage::MetaClient<'_>,
+                status: operon::schema_base::TicketStatus,
+            ) -> Result<Vec<BetaTicket>, operon::meta_storage::MetaStorageError> {
+                let schema_prefix = client.schema_prefix();
+                let stmt = format!("SELECT * FROM {schema_prefix}ticket_beta WHERE status = $1;");
+                let rows = client.query(&stmt, &[&status]).await?;
+                let jobs = rows
+                    .iter()
+                    .map(|row| <BetaTicket as operon::schema_base::TicketSql>::from_sql_row(row))
+                    .collect::<Result<Vec<_>, _>>()?;
+                Ok(jobs)
+            }
+        };
+
+        assert_eq!(tokens.to_string(), expected.to_string());
+    }
+}
