9353476f10f74e377f8fcc4aa2d2235c73593c3f
coinmoles <andrewpark421@gmail.com>
2025-07-04 07:23:35 +0000

    chore: fix typo

--- macros/src/configs/entity.rs
+++ macros/src/configs/entity.rs
@@ -5,7 +5,7 @@ pub type EntityId = String;
 /// An Operon entity, which can be a struct or an enum.
 #[derive(Debug)]
 pub struct EntityConfig {
-    /// Unique identifier for the dimension.
+    /// Unique identifier for the entity.
     pub id: EntityId,
     /// Dimensions this entity repeat on.
     pub dims: Vec<DimensionId>,
