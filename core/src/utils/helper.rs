06b72ee54f4dbc1bd6a6ab8bd37ba848bbb84ad0
coinmoles <andrewpark421@gmail.com>
2025-07-07 05:41:49 +0000

    chore: move `OptionExt` from `schema_base` to `helper`

--- core/src/utils/helper.rs
+++ core/src/utils/helper.rs
@@ -1,3 +1,5 @@
+use std::num::TryFromIntError;
+
 pub trait SplitFirstOwned<T> {
     fn split_first_owned(self) -> Option<(T, Vec<T>)>;
 }
@@ -9,5 +11,18 @@ impl<T, I: IntoIterator<Item = T>> SplitFirstOwned<T> for I {
         let first = iter.next()?;
         let rest: Vec<T> = iter.collect();
         Some((first, rest))
+    }
+}
+
+pub trait OptionExt<T> {
+    fn resolve(&self) -> Result<i64, TryFromIntError>;
+}
+
+impl OptionExt<i64> for Option<usize> {
+    fn resolve(&self) -> Result<i64, TryFromIntError> {
+        match self {
+            Some(i) => i64::try_from(*i),
+            None => Ok(-1),
+        }
     }
 }
