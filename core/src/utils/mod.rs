92b1d5b04ada46e31fed5be141beb50aa1e8491c
coinmoles <andrewpark421@gmail.com>
2025-07-11 09:46:03 +0000

    chore: move `SchemaPrefix` to a `utils` module

--- core/src/utils/mod.rs
+++ core/src/utils/mod.rs
@@ -1,2 +1,5 @@
 mod helper;
 pub use helper::*;
+
+mod schema_prefix;
+pub use schema_prefix::*;
---
+++ core/src/utils/schema_prefix.rs
@@ -0,0 +1,11 @@
+pub struct SchemaPrefix<'a>(pub Option<&'a str>);
+
+impl std::fmt::Display for SchemaPrefix<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if let Some(schema) = self.0 {
+            write!(f, "{schema}.")
+        } else {
+            Ok(())
+        }
+    }
+}
