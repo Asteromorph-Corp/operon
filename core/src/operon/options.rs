e228fe48409bead1298f6821091c998b09d082df
coinmoles <andrewpark421@gmail.com>
2025-07-11 09:59:03 +0000

    refactor: make option structs easier to use

--- core/src/operon/options.rs
+++ core/src/operon/options.rs
@@ -4,31 +4,82 @@ use crate::{scheduler::SchedulerOptions, ui::LogOptions};
 
 pub struct OperonOptions {
     // Scheduler options
-    pub internal_channel_size: usize,
-    // Storage options
-    pub storage_uri: String,
-    pub storage_pool_size: u32,
-    pub storage_schema: Option<String>,
-    pub storage_connection_timeout: std::time::Duration,
+    pub(crate) internal_channel_size: usize,
     // Meta storage options
-    pub meta_storage_uri: String,
-    pub meta_storage_pool_size: u32,
-    pub meta_storage_schema: Option<String>,
-    pub meta_storage_connection_timeout: std::time::Duration,
+    pub(crate) meta_storage_uri: String,
+    pub(crate) meta_storage_pool_size: u32,
+    pub(crate) meta_storage_schema: Option<String>,
+    pub(crate) meta_storage_keepalives_idle: std::time::Duration,
+    pub(crate) meta_storage_keepalives_interval: std::time::Duration,
     // Log options
-    pub log_level: log::Level,
-    pub log_buffer_size: usize,
-    pub log_dump: Option<String>,
+    pub(crate) log_level: log::Level,
+    pub(crate) log_buffer_size: usize,
+    pub(crate) log_dump: Option<String>,
 }
 
 impl OperonOptions {
-    pub fn split(self) -> (SchedulerOptions, LogOptions) {
+    pub fn new(meta_storage_uri: impl Into<String>) -> Self {
+        Self {
+            internal_channel_size: 1024,
+            meta_storage_uri: meta_storage_uri.into(),
+            meta_storage_pool_size: 16,
+            meta_storage_schema: Some("metadata".to_string()),
+            meta_storage_keepalives_idle: std::time::Duration::from_secs(60),
+            meta_storage_keepalives_interval: std::time::Duration::from_secs(30),
+            log_level: log::Level::Info,
+            log_buffer_size: 1024,
+            log_dump: Some("./logs".to_string()),
+        }
+    }
+
+    pub fn with_internal_channel_size(mut self, size: usize) -> Self {
+        self.internal_channel_size = size;
+        self
+    }
+
+    pub fn with_meta_storage_pool_size(mut self, size: u32) -> Self {
+        self.meta_storage_pool_size = size;
+        self
+    }
+
+    pub fn with_meta_storage_schema(mut self, schema: Option<String>) -> Self {
+        self.meta_storage_schema = schema;
+        self
+    }
+
+    pub fn with_meta_storage_keepalives_idle(mut self, duration: std::time::Duration) -> Self {
+        self.meta_storage_keepalives_idle = duration;
+        self
+    }
+
+    pub fn with_meta_storage_keepalives_interval(mut self, duration: std::time::Duration) -> Self {
+        self.meta_storage_keepalives_interval = duration;
+        self
+    }
+
+    pub fn with_log_level(mut self, level: log::Level) -> Self {
+        self.log_level = level;
+        self
+    }
+
+    pub fn with_log_buffer_size(mut self, size: usize) -> Self {
+        self.log_buffer_size = size;
+        self
+    }
+
+    pub fn with_log_dump(mut self, dump: Option<String>) -> Self {
+        self.log_dump = dump;
+        self
+    }
+
+    pub(crate) fn split(self) -> (SchedulerOptions, LogOptions) {
         let scheduler_options = SchedulerOptions {
             internal_channel_size: self.internal_channel_size,
             database_uri: self.meta_storage_uri,
             pool_size: self.meta_storage_pool_size as usize,
-            connection_timeout: self.meta_storage_connection_timeout,
             schema: self.meta_storage_schema,
+            keepalives_idle: self.meta_storage_keepalives_idle,
+            keepalives_interval: self.meta_storage_keepalives_interval,
         };
         let log_options = LogOptions {
             level: self.log_level,
