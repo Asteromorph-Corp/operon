f4f802138649e73c9f37cce8bcc7dc7b37bdf134
coinmoles <andrewpark421@gmail.com>
2025-07-11 02:20:36 +0000

    chore: add test for multi dimensional batch get

--- macros/src/macros/core/trait_storage.rs
+++ macros/src/macros/core/trait_storage.rs
@@ -302,6 +302,98 @@ mod tests {
     }
 
     #[test]
+    fn test_batch_gets_with_multiple_over() {
+        let entities = EntityConfigMap::from_iter([
+            (
+                "b".to_string(),
+                EntityConfig {
+                    id: "b".to_string(),
+                    dims: vec!["i".to_string(), "j".to_string()],
+                    body: String::new(),
+                },
+            ),
+            (
+                "d".to_string(),
+                EntityConfig {
+                    id: "d".to_string(),
+                    dims: vec!["i".to_string(), "j".to_string(), "k".to_string()],
+                    body: String::new(),
+                },
+            ),
+            (
+                "e".to_string(),
+                EntityConfig {
+                    id: "e".to_string(),
+                    dims: vec!["i".to_string(), "l".to_string()],
+                    body: String::new(),
+                },
+            ),
+        ]);
+        let jobs = JobConfigMap::from_iter([(
+            "epsilon".to_string(),
+            JobConfig {
+                id: "epsilon".to_string(),
+                from: vec![
+                    JobArg {
+                        id: "b".to_string(),
+                        over: vec!["j".to_string()],
+                    },
+                    JobArg {
+                        id: "d".to_string(),
+                        over: vec!["j".to_string(), "k".to_string()],
+                    },
+                ],
+                to: "e".to_string(),
+                dims: vec!["i".to_string()],
+                spawn_dim: Some("l".to_string()),
+            },
+        )]);
+
+        let item = batch_gets(&jobs, &entities).collect::<Vec<_>>();
+        let expected: Vec<syn::TraitItemFn> = vec![
+            parse_quote! {
+                async fn get_all_b_over_j(&self, i: schema::IDim) -> Result<Vec<B>, operon::storage::StorageError> {
+                    let final_results = {
+                        let mut results_0 = Vec::new();
+                        let mut j = 0usize;
+                        while let Some(value) = self.get_b(i, j.into()).await? {
+                            results_0.push(value);
+                            j += 1;
+                        }
+                        (!results_0.is_empty()).then_some(results_0)
+                    };
+                    Ok(final_results.unwrap_or_default())
+                }
+            },
+            parse_quote! {
+                async fn get_all_d_over_jk(&self, i: schema::IDim) -> Result<Vec<Vec<D>>, operon::storage::StorageError> {
+                    let final_results = {
+                        let mut results_0 = Vec::new();
+                        let mut j = 0usize;
+                        while let Some(value) = {
+                            let mut results_1 = Vec::new();
+                            let mut k = 0usize;
+                            while let Some(value) = self.get_d(i, j.into(), k.into()).await? {
+                                results_1.push(value);
+                                k += 1;
+                            }
+                            (!results_1.is_empty()).then_some(results_1)
+                        }
+                        {
+                            results_0.push(value);
+                            j += 1;
+                        }
+                        (!results_0.is_empty()).then_some(results_0)
+                    };
+                    Ok(final_results.unwrap_or_default())
+                }
+            },
+        ];
+
+        assert_eq!(item, expected);
+    }
+
+    #[test]
     fn test_batch_inserts() {
         let jobs = JobConfigMap::from_iter([
             (
