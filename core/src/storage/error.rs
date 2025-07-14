ce160d5783a768a826a057218510a50a085a949e
coinmoles <andrewpark421@gmail.com>
2025-07-14 03:06:06 +0000

    fix: add additional error variants to StorageError enum

--- core/src/storage/error.rs
+++ core/src/storage/error.rs
@@ -1,3 +1,5 @@
+use std::num::TryFromIntError;
+
 use thiserror::Error;
 
 #[derive(Debug, Error)]
@@ -10,6 +12,10 @@ pub enum StorageError {
     PoolBuildError(#[from] deadpool_postgres::BuildError),
     #[error("Database uri parse error: {0}")]
     DatabaseUriParseError(String),
+    #[error("Integer conversion error: {0}")]
+    IntegerConversionError(#[from] TryFromIntError),
+    #[error("Error serializing/deserializing json: {0}")]
+    JsonError(#[from] serde_json::Error),
     #[error("Not found: {0}")]
     NotFound(String),
     #[error("Other error: {0}")]
