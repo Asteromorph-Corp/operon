d9c0815d948ec03863008f946b99c4d06e10b607
coinmoles <andrewpark421@gmail.com>
2025-07-14 06:44:27 +0000

    chore: add doc comment to type alias for sql storage

--- macros/src/macros/prelude.rs
+++ macros/src/macros/prelude.rs
@@ -24,6 +24,9 @@ pub fn prelude(all_configs: &AllConfig) -> syn::ItemMod {
 
             pub use traits::{#service_trait, #storage_trait};
 
+            /// The SQL storage that can be used with the service.
+            ///
+            /// This can only be used when all entities implement `Serialize` and `DeserializeOwned`.
             pub type #sql_storage = storage::#sql_storage<#(#generics),*>;
 
             pub fn #handler_ident<Svc: #service_trait, Sto: #storage_trait>() -> #operon::scheduler::SchedulerHandler<Svc, Sto> {
