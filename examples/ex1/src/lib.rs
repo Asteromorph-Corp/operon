7951e13e203aa2927bfca187971800cb73cbfa52
coinmoles <andrewpark421@gmail.com>
2025-07-14 08:27:03 +0000

    chore: add examples

--- examples/ex1/src/lib.rs
+++ examples/ex1/src/lib.rs
@@ -1,53 +1,81 @@
-// use operon::include_operon;
+use operon::serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(crate = "operon::serde")]
+pub struct A(pub String);
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(crate = "operon::serde")]
+pub struct B(pub A, pub usize);
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(crate = "operon::serde")]
+pub struct C(pub usize);
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(crate = "operon::serde")]
+pub struct D {
+    pub a: A,
+    pub b: B,
+    pub c: C,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(crate = "operon::serde")]
+pub struct E {
+    pub b: Vec<B>,
+    pub d: Vec<D>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(crate = "operon::serde")]
+pub enum F {
+    Success { c: Vec<C>, e: Vec<E> },
+    Failure(String, Option<C>, Option<E>),
+}
 
 // include_operon! {
-//     config = {
-//         storage.data.uri = "postgres://user:password@hostname:port/operon-db",
-//         storage.data.pool_size = 16,
-//         storage.data.schema = "data",
-//         storage.metadata.uri = "postgres://user:password@hostname:port/operon-db",
-//         storage.data.schema = "metadata",
-//         log.level = "debug",
-//         log.buffer_size = 1024,
-//         log.dump = true,
-//         log.dump_path = "logs",
-//     };
-//     types = {
-//         #[entity(primary, dims = ["i"])]
-//         pub struct A(pub String);
+//     A<i> -- beta --> B<i, j>
 
-//         #[entity(dims = ["i", "j"], def = "beta|i", from = ["A"], pool = 8)]
-//         pub struct B(pub A, pub usize);
+//     #[entity(primary, dims = ["i"])]
 
-//         #[entity(dims = ["i", "k"], def = "gamma|i", from = ["A"], pool = 8)]
-//         pub struct C(pub usize);
+//     #[entity(dims = ["i", "j"], def = "beta", from = ["A"], pool = 8)]
 
-//         #[entity(
-//             dims = ["i", "j", "k"],
-//             def = "delta | i, j, k",
-//             from = ["A", "B", "C"],
-//             pool = 4
-//         )]
-//         pub struct D {
-//             pub a: A,
-//             pub b: B,
-//             pub c: C,
-//         }
+//     #[entity(dims = ["i", "k"], def = "gamma", from = ["A"], pool = 8)]
+//     pub struct C(pub usize);
 
-//         #[entity(dims = ["i", "k"], def = "epsilon | i, k", from = ["B | j", "D|j"], pool = 4)]
-//         pub struct E {
-//             pub b: Vec<B>,
-//             pub d: Vec<D>,
-//         }
+//     #[entity(
+//         dims = ["i", "j", "k"],
+//         def = "delta | i, j, k",
+//         from = ["A", "B", "C"],
+//         pool = 4
+//     )]
+//     pub struct D {
+//         pub a: A,
+//         pub b: B,
+//         pub c: C,
+//     }
 
-//         #[entity(dims = ["i"], def = "zeta|i", from = ["C|k", "E|k"])]
-//         pub enum F {
-//             Success {
-//                 c: Vec<C>,
-//                 e: Vec<E>,
-//             },
-//             Failure(String, Option<C>, Option<E>),
-//         }
-//     };
-//     use_psql_storage = DataStorage;
+//     B<i, j> --> epsilon
+//     D<i, j, k> --> epsilon
+//     epsilon --> E<i, k>
+
+//     #[entity(dims = ["i", "k"], def = "epsilon | i, k", from = ["B | j", "D|j"], pool = 4)]
+//     pub struct E {
+//         pub b: Vec<B>,
+//         pub d: Vec<D>,
+//     }
+
+//     #[entity(dims = ["i"], def = "zeta|i", from = ["C|k", "E|k"])]
+//     pub enum F {
+//         Success {
+//             c: Vec<C>,
+//             e: Vec<E>,
+//         },
+//         Failure(String, Option<C>, Option<E>),
+//     }
 // }
+
+use operon::sample_operon;
+
+sample_operon!();
--- examples/ex1/src/main.rs
+++ examples/ex1/src/main.rs
@@ -1,6 +1,150 @@
+use std::sync::Arc;
+
+use ex1::{
+    A, B, C, CookingService, CookingStorage, D, E, F, PsqlCookingStorage, cooking_handler, schema,
+};
+use operon::{
+    async_trait::async_trait,
+    operon::{Operon, OperonOptions},
+    service::OperonService,
+    storage::{OperonStorage, StorageOptions},
+};
+use rand::Rng;
+
+// Example minimal random service implementation
+struct ExampleService;
+// Implement the OperonService trait
+// This must always be implemented by the user.
+#[async_trait]
+impl OperonService for ExampleService {
+    type JobEnum = schema::JobEnum;
+    type ResolutionEnum = schema::ResolutionEnum;
+}
+
+#[async_trait]
+impl CookingService for ExampleService {
+    async fn beta(&self, a: A) -> Result<Vec<B>, Box<dyn std::error::Error + Send + Sync>> {
+        // Simulate some processing
+        // Poison this function to simulate a failure
+        // let mut rng = rand::rng();
+        // if rng.random_bool(0.0005) {
+        //     return Err(anyhow::anyhow!("Simulated failure in beta"));
+        // }
+        let mut result = Vec::new();
+        let mut rng = rand::rng();
+        for i in 0..rng.random_range(5..=10) {
+            let b = B(a.clone(), a.0.len() + i);
+            result.push(b);
+        }
+        Ok(result)
+    }
+
+    async fn gamma(&self, a: A) -> Result<Vec<C>, Box<dyn std::error::Error + Send + Sync>> {
+        // Simulate some processing
+        // Poison this function to simulate a failure
+        // let mut rng = rand::rng();
+        // if rng.random_bool(0.0005) {
+        //     return Err(anyhow::anyhow!("Simulated failure in gamma"));
+        // }
+        let mut result = Vec::new();
+        let mut rng = rand::rng();
+        let len = a.0.len();
+        for _ in 0..rng.random_range(5..=10) {
+            let c = C(rng.random_range(1..=len));
+            result.push(c);
+        }
+        Ok(result)
+    }
+
+    async fn delta(&self, a: A, b: B, c: C) -> Result<D, Box<dyn std::error::Error + Send + Sync>> {
+        // // Simulate some processing
+        // // Poison this function to simulate a failure
+        // let mut rng = rand::rng();
+        // if rng.random_bool(0.0005) {
+        //     // panic!("Simulated failure in delta");
+        //     return Err(anyhow::anyhow!("Simulated failure in delta"));
+        // }
+        let d = D {
+            a: a.clone(),
+            b: b.clone(),
+            c: c.clone(),
+        };
+        // // Printing logs should be done using the macros `operon` provides.
+        // // For example:
+        // let mut rng = rand::rng();
+        // if rng.random_bool(0.001) {
+        //     operon::info!("Sample delta: {d:?}");
+        // }
+        log::trace!("Delta computed: {d:?}");
+        Ok(d)
+    }
+
+    async fn epsilon(
+        &self,
+        b_j: Vec<B>,
+        d_j: Vec<D>,
+    ) -> Result<E, Box<dyn std::error::Error + Send + Sync>> {
+        // Simulate some processing
+        // Poison this function to simulate a failure
+        // let mut rng = rand::rng();
+        // if rng.random_bool(0.0005) {
+        //     return Err(anyhow::anyhow!("Simulated failure in epsilon"));
+        // }
+        let e = E {
+            b: b_j.to_vec(),
+            d: d_j.to_vec(),
+        };
+        Ok(e)
+    }
+
+    async fn zeta(
+        &self,
+        c_k: Vec<C>,
+        e_k: Vec<E>,
+    ) -> Result<F, Box<dyn std::error::Error + Send + Sync>> {
+        // Simulate some processing
+        // Poison this function to simulate a failure
+        // let mut rng = rand::rng();
+        // if rng.random_bool(0.0005) {
+        //     return Err(anyhow::anyhow!("Simulated failure in zeta"));
+        // }
+        let mut rng = rand::rng();
+        let f = if rng.random_bool(0.5) {
+            F::Success {
+                c: c_k.to_vec(),
+                e: e_k.to_vec(),
+            }
+        } else {
+            F::Failure(
+                format!("F (failure with {} C and {} E)", c_k.len(), e_k.len()),
+                c_k.to_vec().first().cloned(),
+                e_k.to_vec().first().cloned(),
+            )
+        };
+        Ok(f)
+    }
+}
+
 #[tokio::main]
-async fn main() {
-    // Placeholder
-    let var = std::env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR not set");
-    println!("{var}");
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let database_uri = std::env::var("POSTGRES_URI")?;
+    let primary_ub = 50;
+
+    let operon_options = OperonOptions::new(&database_uri);
+    let storage_options = StorageOptions::new(database_uri);
+
+    let service = Arc::new(ExampleService);
+    let storage = Arc::new(PsqlCookingStorage::new(storage_options)?);
+
+    storage.init().await?;
+    for i in 0..primary_ub {
+        let a = A(format!("A ({i})"));
+        storage.put_a(i, a.clone()).await?;
+    }
+
+    Operon::new(service, storage, operon_options)
+        .run(cooking_handler(), primary_ub)
+        .await?;
+
+    Ok(())
 }
