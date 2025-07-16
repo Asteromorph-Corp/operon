012460a55f4edf23cfe843f641a5a80e1a73b357
crescmoon <crescentmoon8569@gmail.com>
2025-07-16 14:18:47 +0900

    chore: remove commented-out code and update sample_operon macro usage

--- examples/ex1/src/lib.rs
+++ examples/ex1/src/lib.rs
@@ -34,48 +34,14 @@ pub enum F {
     Failure(String, Option<C>, Option<E>),
 }
 
-// include_operon! {
-//     A<i> -- beta --> B<i, j>
-
-//     #[entity(primary, dims = ["i"])]
-
-//     #[entity(dims = ["i", "j"], def = "beta", from = ["A"], pool = 8)]
-
-//     #[entity(dims = ["i", "k"], def = "gamma", from = ["A"], pool = 8)]
-//     pub struct C(pub usize);
-
-//     #[entity(
-//         dims = ["i", "j", "k"],
-//         def = "delta | i, j, k",
-//         from = ["A", "B", "C"],
-//         pool = 4
-//     )]
-//     pub struct D {
-//         pub a: A,
-//         pub b: B,
-//         pub c: C,
-//     }
-
-//     B<i, j> --> epsilon
-//     D<i, j, k> --> epsilon
-//     epsilon --> E<i, k>
-
-//     #[entity(dims = ["i", "k"], def = "epsilon | i, k", from = ["B | j", "D|j"], pool = 4)]
-//     pub struct E {
-//         pub b: Vec<B>,
-//         pub d: Vec<D>,
-//     }
-
-//     #[entity(dims = ["i"], def = "zeta|i", from = ["C|k", "E|k"])]
-//     pub enum F {
-//         Success {
-//             c: Vec<C>,
-//             e: Vec<E>,
-//         },
-//         Failure(String, Option<C>, Option<E>),
-//     }
-// }
-
 use operon::sample_operon;
 
-sample_operon!();
+sample_operon! {
+    Cooking = |A<i>| {
+        B<j> = beta(A) for(8) i;
+        C<k> = gamma(A) for(8) i;
+        D    = delta(A, B, C) for(4) i, j, k;
+        E    = epsilon(B<j>, D<j>) for(4) i, k;
+        F    = zeta(C<k>, E<k>) for i;
+    }
+}
