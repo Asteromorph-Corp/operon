6ac7012b361ceb0341bcec266d9cd80e7e640354
crescmoon <crescentmoon8569@gmail.com>
2025-07-18 15:37:04 +0900

    feat: include generated doc comment in service trait

--- macros/src/macros/core/trait_service.rs
+++ macros/src/macros/core/trait_service.rs
@@ -1,5 +1,3 @@
-use syn::parse_quote;
-
 use crate::{
     AllConfig,
     utils::{
@@ -7,6 +5,8 @@ use crate::{
         resolution_enum_ident, service_trait_ident,
     },
 };
+use quote::ToTokens;
+use syn::parse_quote;
 
 /// Generates a trait for the service based on the provided AllConfig.
 ///
@@ -30,7 +30,7 @@ pub fn trait_service(all_configs: &AllConfig) -> syn::ItemTrait {
     let res_enum_ident = resolution_enum_ident();
     let svc_ident = service_trait_ident(&all_configs.service_id);
 
-    let job_fns = all_configs.jobs.values().map(|job| -> syn::TraitItemFn {
+    let (job_fn_strings, job_fns) = all_configs.jobs.values().map(|job| -> (String, syn::TraitItemFn) {
         let fn_name = job_fn_ident(&job.id);
         let args = job.from.iter().map(|arg| -> syn::FnArg {
             let arg_ident = entity_over_dim_ident(&arg.id, &arg.over);
@@ -40,22 +40,58 @@ pub fn trait_service(all_configs: &AllConfig) -> syn::ItemTrait {
                 |acc, _| parse_quote! { Vec<#acc> },
             );
 
-            parse_quote! { #arg_ident: #ty}
+            parse_quote! { #arg_ident: #ty }
         });
         let entity_ident = entity_ident(&job.to);
         let return_ty: syn::Type = job.spawn_dim.as_ref().map_or_else(
             || parse_quote! { #entity_ident },
             |_| parse_quote! { Vec<#entity_ident> },
         );
+        (
+            format!(
+                "async fn {fn_name}(&self, {}) -> Result<{}, Box<dyn std::error::Error + Send + Sync>>;",
+                args.clone().map(|arg| arg.to_token_stream().to_string()).collect::<Vec<_>>().join(", "),
+                return_ty.to_token_stream().to_string().replace(" ", "")
+            ),
+            parse_quote! {
+                async fn #fn_name(&self, #(#args,)*) -> Result<#return_ty, Box<dyn std::error::Error + Send + Sync>>;
+            },
+        )
+    }).unzip::<_, _, Vec<_>, Vec<_>>();
 
-        parse_quote! {
-            async fn #fn_name(&self, #(#args,)*) -> Result<#return_ty, Box<dyn std::error::Error + Send + Sync>>;
-        }
-    });
+    let doc_comment = format!(
+        "# {svc_ident}\n\n\
+        This trait defines the functions Operon will call \
+        to execute jobs in the service you defined. \
+        Implement the functions provided below to define \
+        the behaviour of your service.\n\n\
+        Notes:\n\n\
+        * All functions are async methods and must return a `Result<_, Box<dyn std::error::Error + Send + Sync>>`.\n\
+        * Repeated parameters are slices; repeated return types are vectors.\n\
+        * The functions in the trait are intended to be stateless. \
+          The struct may contain static metadata, but you would need to implement \
+          internally-mutable thread-safe states if you really need stateful functions.\n\n\
+        Please consult the following section for the exact function signatures.\n\n\
+        ## Function Signatures\n\n\
+        The functions were parsed as follows:\n\n\
+        ```rust,ignore\n\
+        use operon::async_trait::async_trait;\n\
+        #[async_trait]\n\
+        pub trait {svc_ident} {{\n\
+        {job_fns}\n\
+        }}\n\
+        ```",
+        job_fns = job_fn_strings
+            .iter()
+            .map(|fn_body| format!("    {fn_body}"))
+            .collect::<Vec<_>>()
+            .join("\n")
+    );
 
     parse_quote! {
         #[#operon::async_trait::async_trait]
         #[automatically_derived]
+        #[doc = #doc_comment]
         pub trait #svc_ident: #operon::service::OperonService<JobEnum = schema::#job_enum_ident, ResolutionEnum = schema::#res_enum_ident> {
             #(#job_fns)*
         }
@@ -175,10 +211,11 @@ mod tests {
             ]),
         };
 
-        let result = trait_service(&all_configs);
+        let result = syn::ItemTrait {
+            attrs: vec![],
+            ..trait_service(&all_configs)
+        };
         let expected: syn::ItemTrait = parse_quote! {
-            #[operon::async_trait::async_trait]
-            #[automatically_derived]
             pub trait CookingService: operon::service::OperonService<JobEnum = schema::JobEnum, ResolutionEnum = schema::ResolutionEnum> {
                 async fn beta(&self, a: A,) -> Result<Vec<B>, Box<dyn std::error::Error + Send + Sync>>;
                 async fn gamma(&self, a: A,) -> Result<Vec<C>, Box<dyn std::error::Error + Send + Sync>>;
