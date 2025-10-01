6663cdb460a44b9fdd02c91bf9329f0cb08befd6
Jiho Park <andrewpark421@gmail.com>
2025-10-01 10:23:17 +0000

    chore: add TODO item

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -9,6 +9,8 @@ use quote::quote;
 use crate::configs::{AllConfig, DimensionConfig, EntityConfig, JobArg, JobConfig, JobConfigMap};
 use crate::macros::operon;
 
+// TODO: use text fixtures instead of constructing the configs in code.
+
 #[proc_macro]
 pub fn define_operon(input: TokenStream) -> TokenStream {
     let all_configs: AllConfig = match syn::parse(input) {
