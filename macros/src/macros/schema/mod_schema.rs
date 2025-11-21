5c503a62eefd791b8b067b83b5067a71e0885eca
Jiho Park <andrewpark421@gmail.com>
2025-11-21 05:34:48 +0000

    refactor: rename variables

--- macros/src/macros/schema/mod_schema.rs
+++ macros/src/macros/schema/mod_schema.rs
@@ -6,20 +6,20 @@ use crate::macros::schema::resolution::{impl_resolution_enum, resolution_enum_de
 
 /// Generates the `mod schema` module with all schema-related items.
 pub fn mod_schema(all_configs: &AllConfig) -> syn::ItemMod {
-    let job_enum = job_enum_definition(&all_configs.jobs);
+    let job_enum_def = job_enum_definition(&all_configs.jobs);
     let impl_job_enum = impl_job_enum();
 
-    let resolution_enum = resolution_enum_definition(&all_configs.dimensions);
+    let resolution_enum_def = resolution_enum_definition(&all_configs.dimensions);
     let impl_resolution_enum = impl_resolution_enum();
 
     parse_quote! {
         pub mod schema {
             use super::*;
 
-            #job_enum
+            #job_enum_def
             #impl_job_enum
 
-            #resolution_enum
+            #resolution_enum_def
             #impl_resolution_enum
         }
     }
