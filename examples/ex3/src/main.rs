79744cc3a50afa6d9141ecb0a5de253bb2b19c20
crescmoon <crescentmoon8569@gmail.com>
2025-12-15 16:46:39 +0900

    example: add example 3 for stress testing

---
+++ examples/ex3/src/main.rs
@@ -0,0 +1,115 @@
+use rand::Rng;
+
+type A = ();
+type B = ();
+type C = ();
+type D = ();
+type E = ();
+type F = ();
+type G = ();
+type H = ();
+
+operon::define_operon! {
+    stress_test = {
+        A<i> = alpha();
+        B<j> = beta(A<i>, A) for i;
+        C<k> = gamma(A) for i;
+        D<l> = delta(A, B<i, j>, B<j>, B, C) for i, j, k;
+        E<m> = epsilon(D<i, j, k, l>);
+        F<n> = zeta(E, D<j, l>, D) for j, l, i, k, m;
+        G = eta(F<n>, F<l, n>, F, E, D<l>, D) for n, l, i, k, m, j;
+        H = theta(G, F, E, D, C, B, A) for i, j, k, l, m, n;
+    }
+}
+
+#[derive(operon::OperonService)]
+struct MyService;
+#[operon::async_trait::async_trait]
+impl StressTestService for MyService {
+    async fn alpha(&self) -> Result<Vec<A>, operon::operon::UserError> {
+        let mut rng = rand::rng();
+        let size = rng.random_range(1..5);
+        Ok((0..size).map(|_| ()).collect())
+    }
+    async fn beta(&self, _a_i: Vec<A>, _a: A) -> Result<Vec<B>, operon::operon::UserError> {
+        let mut rng = rand::rng();
+        let size = rng.random_range(1..5);
+        Ok((0..size).map(|_| ()).collect())
+    }
+    async fn gamma(&self, _a: A) -> Result<Vec<C>, operon::operon::UserError> {
+        let mut rng = rand::rng();
+        let size = rng.random_range(1..5);
+        Ok((0..size).map(|_| ()).collect())
+    }
+    async fn delta(
+        &self,
+        _a: A,
+        _b_ij: Vec<Vec<B>>,
+        _b_j: Vec<B>,
+        _b: B,
+        _c: C,
+    ) -> Result<Vec<D>, operon::operon::UserError> {
+        let mut rng = rand::rng();
+        let size = rng.random_range(1..5);
+        Ok((0..size).map(|_| ()).collect())
+    }
+    async fn epsilon(
+        &self,
+        _d_ijkl: Vec<Vec<Vec<Vec<D>>>>,
+    ) -> Result<Vec<E>, operon::operon::UserError> {
+        let mut rng = rand::rng();
+        let size = rng.random_range(1..5);
+        Ok((0..size).map(|_| ()).collect())
+    }
+    async fn zeta(
+        &self,
+        _e: E,
+        _d_jl: Vec<Vec<D>>,
+        _d: D,
+    ) -> Result<Vec<F>, operon::operon::UserError> {
+        let mut rng = rand::rng();
+        let size = rng.random_range(1..5);
+        Ok((0..size).map(|_| ()).collect())
+    }
+    async fn eta(
+        &self,
+        _f_n: Vec<F>,
+        _f_ln: Vec<Vec<F>>,
+        _f: F,
+        _e: E,
+        _d_l: Vec<D>,
+        _d: D,
+    ) -> Result<G, operon::operon::UserError> {
+        Ok(())
+    }
+    async fn theta(
+        &self,
+        _g: G,
+        _f: F,
+        _e: E,
+        _d: D,
+        _c: C,
+        _b: B,
+        _a: A,
+    ) -> Result<H, operon::operon::UserError> {
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let database_uri = std::env::var("POSTGRES_URI")?;
+
+    let storage_options =
+        operon::storage::StorageOptions::new(&database_uri).with_schema("ex3_data");
+    let operon_options =
+        operon::operon::OperonOptions::new(&database_uri).with_meta_storage_schema("ex3_meta");
+
+    let service = MyService;
+    let storage = PsqlStressTestStorage::new(storage_options)?;
+    operon::operon::Operon::new(service, storage, operon_options)
+        .run()
+        .await?;
+
+    Ok(())
+}
