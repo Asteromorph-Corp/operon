f2dda449054e058d0bc4a6c7a1e0a71da334b3a0
Sungbin Moon <73477326+crescmoon@users.noreply.github.com>
2026-04-28 11:59:15 +0900

    Revert "Release v0.2.3 (#35)"
    This reverts commit e7fefdbb5ec0770a3503ee73fb4f4aeaa9dcf92b.

--- operon/src/scheduler/individual_scheduler.rs
+++ operon/src/scheduler/individual_scheduler.rs
@@ -89,7 +89,7 @@ where
     ) -> Result<(), SchedulerError> {
         let (done, queued, waiting) = client.ticket(self.meta).get_status().await?;
         let finished = (*self.progress.write().await).update(done, queued, waiting);
-        if finished && self.state != TaskState::Finished {
+        if finished {
             self.state = TaskState::Finished;
             tracing::info!("All `{}` jobs are finished.", self.meta.id);
         }
