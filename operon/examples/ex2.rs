ea6605007d70d7f99075d84437e7b89f61a9d19d
Jiho Park <73594851+coinmoles@users.noreply.github.com>
2026-07-15 17:51:19 +0900

    chore(ci): update Rust workflow (#73)
    * chore(ci): update Rust workflow
    
    * chore(ci): add examples job
    
    * chore: specify correct minimal version for dependencies
    
    * chore: move examples under operon
    
    * fix(codegen): make `FoundCrate::Itself` resolve to `operon`
    
    * chore(ci): remove examples job
    
    * chyore: specify correct minimal version for dev dependencies
    
    * chore: specify correct minimal version for `serde`
    
    * doc: update README examples location

--- operon/examples/ex2.rs
+++ operon/examples/ex2.rs
@@ -1,13 +1,51 @@
 use async_trait::async_trait;
-use ex2::{A, B, C, CookingService, D, E, F, PsqlCookingStorage};
 use operon::error::UserError;
 use operon::options::{OperonOptions, PsqlStorageOptions};
-use operon::{Operon, OperonService};
+use operon::{Operon, OperonService, define_operon};
 use rand::Rng;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct A(pub String);
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct B(pub A, pub usize);
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct C(pub usize);
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct D {
+    pub a: A,
+    pub b: B,
+    pub c: C,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct E {
+    pub b: Vec<B>,
+    pub d: Vec<D>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum F {
+    Success { c: Vec<C>, e: Vec<E> },
+    Failure(String, Option<C>, Option<E>),
+}
+
+define_operon! {
+    cooking = {
+        A<i> = alpha();
+        B<j> = beta(A) for(8) i;
+        C<k> = gamma(A) for(8) i;
+        D    = delta(A, B, C) for(4) i, j, k;
+        E    = epsilon(B<j>, D<j>) for(4) i, k;
+        F    = zeta(C<k>, E<k>) for i;
+    }
+}
 
 // Example service implementation
 #[derive(OperonService)]
-#[operon(defined_at = "ex2")]
 struct ExampleService;
 
 #[async_trait]
